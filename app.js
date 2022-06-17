f0cfa48f6b08bd051930d0c93ef681cb0c234737
Tim Hrovat <tim.hrovat16@gmail.com>
2022-06-17 15:47:29 +0100

    fixed nav and colors

--- app.js
+++ app.js
@@ -11,10 +11,17 @@ let menuIsShown = false;
 
 let showMenu = () => {
   let element = document.getElementById("menu");
+  let burger = document.getElementById("nav-btn");
   if (menuIsShown) {
     element.classList.remove("active");
+    burger.classList.add("fa-bars");
+    burger.classList.remove("fa-times");
+    burger.style.padding = "7px 9px 7px 9px";
   } else {
     element.classList.add("active");
+    burger.classList.add("fa-times");
+    burger.classList.remove("fa-bars");
+    burger.style.padding = "7px 10px 7px 10px";
   }
   menuIsShown = !menuIsShown;
 };
