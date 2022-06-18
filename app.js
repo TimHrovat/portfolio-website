b96949f077a8e8e299b1f666dd55c584056ed85c
Tim Hrovat <tim.hrovat16@gmail.com>
2022-06-18 14:45:15 +0100

    fixed css added animation

--- app.js
+++ app.js
@@ -25,3 +25,38 @@ let showMenu = () => {
   }
   menuIsShown = !menuIsShown;
 };
+
+function waitForMS(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function write(
+  sentence,
+  elementReference,
+  eachLetterDelayMS,
+  underscoreReference,
+  displayUnderscoreAfterAnimationForMS
+) {
+  const underscore = document.getElementById(underscoreReference);
+  underscore.style.display = "inline-block";
+
+  const letters = sentence.split("");
+
+  for (let i = 0; i < letters.length; i++) {
+    await waitForMS(eachLetterDelayMS);
+    document.getElementById(elementReference).append(letters[i]);
+  }
+
+  await waitForMS(displayUnderscoreAfterAnimationForMS);
+  underscore.style.display = "none";
+
+  return;
+}
+
+async function typewriter() {
+  await write("Hey, I'm", "hey", 130, "underscore1", 0);
+  await write("Tim Hrovat", "name", 130, "underscore2", 5000);
+  return;
+}
+
+typewriter();
